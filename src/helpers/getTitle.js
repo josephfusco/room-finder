60a690f21936d15c90228071b0cacbccfbe5a432
Joseph Fusco <jfusco@masonite.com>
2020-03-07 23:54:15 -0500

    Remove console logs

--- src/helpers/getTitle.js
+++ src/helpers/getTitle.js
@@ -3,9 +3,7 @@ import rooms from "rooms";
 function getTitle(pathname) {
   const pathnameNoSlash = pathname.replace(/\/+$/, "");
   const room = rooms.filter(room => room.slug === pathnameNoSlash)[0];
-  console.log(room);
   const { title } = room || { title: "Room Finder" };
-  console.log(room);
 
   return title;
 }
