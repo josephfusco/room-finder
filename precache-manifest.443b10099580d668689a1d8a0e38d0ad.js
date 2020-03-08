3d93021e01f12aad5480c95e1a601892d8a6f47a
Joseph Fusco <jfusco@masonite.com>
2020-03-07 23:03:40 -0500

    Updates

---
+++ precache-manifest.443b10099580d668689a1d8a0e38d0ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b772b4a3a495c611f12f2deafa0f7342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84042a0e8b19bcbd45b4",
+    "url": "/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "068255bf11d462d3067b",
+    "url": "/static/css/main.f9ad304e.chunk.css"
+  },
+  {
+    "revision": "84042a0e8b19bcbd45b4",
+    "url": "/static/js/2.3b61416f.chunk.js"
+  },
+  {
+    "revision": "73e14be25f2ef735d8255fe7184a3586",
+    "url": "/static/js/2.3b61416f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068255bf11d462d3067b",
+    "url": "/static/js/main.18ebeef5.chunk.js"
+  },
+  {
+    "revision": "44ea62c8732a7aa54764",
+    "url": "/static/js/runtime-main.35d3acb0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1186cebce085fd9e12f55cb3a9f3b20.js"
+  "/precache-manifest.443b10099580d668689a1d8a0e38d0ad.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
